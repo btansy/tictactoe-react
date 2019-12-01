bda3a3af04f96427b4ac429b65acb4917734b103
itsrave <ni3fidzialny@gmail.com>
2019-12-01 19:55:31 +0000

    Deploying to gh-pages from master 64e58f67a30a554d3c97fb0635fc82d72afda0a7

---
+++ precache-manifest.11810be6955a36e40d5f157d4f072331.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f3dfe39c3473f6dc596b12c704fc0f",
+    "url": "/tictactoe-react/index.html"
+  },
+  {
+    "revision": "f5210311b1a3742e6618",
+    "url": "/tictactoe-react/static/css/main.a534aaab.chunk.css"
+  },
+  {
+    "revision": "5599d19d4a8aaa44e738",
+    "url": "/tictactoe-react/static/js/2.8e7a7d68.chunk.js"
+  },
+  {
+    "revision": "f5210311b1a3742e6618",
+    "url": "/tictactoe-react/static/js/main.27caf1e8.chunk.js"
+  },
+  {
+    "revision": "9bd34e890fdd81c3d31d",
+    "url": "/tictactoe-react/static/js/runtime-main.c9dd8e81.js"
+  }
+]);
